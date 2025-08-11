2c1e509b06831504cec3164555a06b01c96ea131
Tapekhin Aleksey <alrexcom@yandex.ru>
2025-08-11 07:29:19 +0000

    visit

---
+++ deploy_visits/files/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+import psycopg2
+import redis
+import os
+
+app = Flask(__name__)
+
+# Подключение к PostgreSQL
+conn = psycopg2.connect(
+    dbname=os.getenv('POSTGRES_DB'),
+    user=os.getenv('app_user'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST')
+)
+
+#conn = psycopg2.connect(
+ #   dbname="visits_db",
+  #  user="app_user",  # Используйте роль postgres
+   # password="visitspass",  # Укажите пароль роли postgres
+   # host="localhost",
+   # port="5432"
+#)
+
+
+
+# Подключение к Redis
+cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, decode_responses=True)
+
+@app.route('/visits')
+def get_visits():
+    # Проверка кэша
+    cached_visits = cache.get('visits')
+    if cached_visits:
+        return jsonify(visits=int(cached_visits), source='cache')
+
+    # Запрос к БД
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO visits DEFAULT VALUES RETURNING id;")
+        visits = cur.execute("SELECT COUNT(*) FROM visits;")
+        conn.commit()
+        count = cur.fetchone()[0]
+        cache.set('visits', count, ex=30)  # Кэш на 30 сек
+        return jsonify(visits=count, source='database')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
